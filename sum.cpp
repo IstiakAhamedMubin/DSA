18ccbbbf9d271779b31e4b5ca7cd11a5edb414a3
Istiak Ahamed <istiakop800@gmail.com>
2026-06-18 10:28:45 +0600

    first commit

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//user input sum calculation
+int main () {
+    int n, sum = 0;
+    cout <<"Enter a positive integer:";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        sum = sum + i;
+
+    }
+    cout << "The sum of first " << n << " positive integers is: " << sum << endl;
+    return 0;
+}
